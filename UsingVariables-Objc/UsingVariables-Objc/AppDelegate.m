53dca5081b204b643e5c0c64ef2040cef14b1909
uumoss <uumoss@gmail.com>
2014-08-28 22:23:06 -0600

    added variables

--- UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
+++ UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
@@ -18,8 +18,18 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    // Declare two variables
+    CGFloat height;
+    CGFloat weight;
+    // Store a value in each variable
+    height = 71.0f;
+    weight = 185.0f;
+    // We'll calculate the body mass index
+    CGFloat bodyMassIndex = (weight / (height * height)) * 703;
+    // Log that to the console
+    NSLog(@"%f",bodyMassIndex);
     
-    
+
     return YES;
 }
 
